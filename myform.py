e7d67cdd0364f3f25a7437e486da1c0d65ffaea2
user1 <user1@A518-03>
2022-03-23 16:27:00 +0300

    MyCommit for something i forgot

--- myform.py
+++ myform.py
@@ -3,4 +3,7 @@ from bottle import post, request
 @post('/home', method='post')
 def my_form():
     mail = request.forms.get('ADRESS')
+    if (mail == None)
+        return ("Adress is null")
     return ("Thanks! The answer will be sent to the mail %s" % mail)
+    
