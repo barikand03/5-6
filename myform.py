18ab3e6ad1e3418f3b106d6f126bd920c1ca485b
Никич <kelmyashkina.o@gmail.com>
2022-03-29 21:12:00 +0300

    Final Mail and Question check

--- myform.py
+++ myform.py
@@ -1,9 +1,22 @@
 from bottle import post, request
+import re
+mailregex = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 
 @post('/home', method='post')
 def my_form():
-    mail = request.forms.get('ADRESS')
-    if (mail == None)
-        return ("Adress is null")
-    return ("Thanks! The answer will be sent to the mail %s" % mail)
-    
+    error = None
+    quest = request.forms.get('QUEST')#Вопрос
+    mail = request.forms.get('ADRESS')#Адрес
+    if quest is None or quest == '' and mail is None or mail == '':#Проверка введенности обоих полей
+        error = "Both fields are empty"
+    if quest is None or quest == '':#Проверка введенности вопроса
+        error = "Empty question field"
+    else:
+        if mail is None or mail == '':#Проверка введенности почты
+            error = "Empty mail field"
+        elif mailregex.match(mail) is None:#Проверка валидности почты
+            error = "Invalid mail address"
+    if error is None:
+        return "Thanks! The answer will be sent to the mail %s" % mail
+    else:
+        return "Oops an error has occurred: %s" % error
